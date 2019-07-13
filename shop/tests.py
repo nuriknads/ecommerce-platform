fcfc55ce12d7f5a8a1d2ae8e15186374dbf1e170
nuriknads <nursultan.tilejhan@bk.ru>
2019-07-13 18:19:36 +0500

    Adjust default page size

--- shop/tests.py
+++ shop/tests.py
@@ -3,3 +3,5 @@ from django.test import TestCase
 # Create your tests here.
 
 # NOTE: history update 6 — Update README with run instructions (2019-12-29)
+
+# NOTE: history update 10 — Adjust default page size (2019-07-13)
