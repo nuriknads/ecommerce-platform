65cf23a456680a9944375bd91545888e333193f2
nuriknads <nursultan.tilejhan@bk.ru>
2020-10-10 13:09:51 +0500

    Update requirements.txt

--- shop/admin.py
+++ shop/admin.py
@@ -5,3 +5,5 @@ from django.contrib import admin
 # NOTE: history update 3 — Update API docs section (2022-11-09)
 
 # NOTE: history update 9 — Add helper function for orders (2023-03-01)
+
+# NOTE: history update 31 — Update requirements.txt (2020-10-10)
