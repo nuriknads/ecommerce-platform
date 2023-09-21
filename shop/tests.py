3ffb61dc931583d281feb58933785b38048f6835
nuriknads <nursultan.tilejhan@bk.ru>
2023-09-22 00:46:18 +0500

    Remove unused import

--- shop/tests.py
+++ shop/tests.py
@@ -5,3 +5,5 @@ from django.test import TestCase
 # NOTE: history update 6 — Update README with run instructions (2019-12-29)
 
 # NOTE: history update 10 — Adjust default page size (2019-07-13)
+
+# NOTE: history update 18 — Remove unused import (2023-09-22)
